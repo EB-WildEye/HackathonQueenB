00eeded3bda332c80ddc19f4315687310cdaad21
shakedN <shnuttman@gmail.com>
2026-01-11 15:15:44 +0200

    fixChat

--- client/src/services/chatService.js
+++ client/src/services/chatService.js
@@ -1,7 +1,7 @@
 import api from "./api";
 
 export default async function sendUserInput(message) {
-  const res = await api.post("/api/chat", { message });
+  const res = await api.post("/api/chat/message", { message });
 
   return res.data;
 }
